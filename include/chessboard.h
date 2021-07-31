7e7f1d40cfd642f603d3fa74b95f125b119e4d9c
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-07-31 21:31:32 +0200

    code review

--- include/chessboard.h
+++ include/chessboard.h
@@ -5,17 +5,17 @@
 
 namespace chessboard {
 
-const float PHYSICAL_BOARD_BLOCK_SIDE_LENGTH = 0.02500f;
-const float PROJECTED_BOARD_BLOCK_SIDE_LENGTH = 0.05400f;
+const float PHYSICAL_BOARD_BLOCK_WIDTH = 0.02500f;
+const float PROJECTED_BOARD_BLOCK_WIDTH = 0.05400f;
 
 void findImageSpaceCorners(std::vector<cv::Mat>& images,
-    std::vector<std::vector<cv::Point2f>>& allFoundCorners,
-    bool showResults = false);
+    std::vector<std::vector<cv::Point2f>>& imageSpaceCorners,
+    bool show = false);
 
-void findWorldSpaceCorners(const cv::Size& boardSize, float squareEdgeLength,
-    std::vector<cv::Point3f>& corners);
+void findWorldSpaceCorners(const cv::Size& boardSize, float blockWidth,
+    std::vector<cv::Point3f>& worldSpaceCorners);
 
-cv::Mat generate(const cv::Size& imgSize, const cv::Size& boardSize,
-    std::vector<cv::Point2f>& corners);
+cv::Mat create(const cv::Size& imgSize, const cv::Size& boardSize,
+    std::vector<cv::Point2f>& imgSpaceCorners);
 }
 #endif // CHESSBOARD_H
