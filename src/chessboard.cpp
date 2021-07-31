8e1e7878c959dfe945cff550b39bfa69f5aadd0f
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-07-31 21:31:48 +0200

    code review

--- src/chessboard.cpp
+++ src/chessboard.cpp
@@ -1,73 +1,67 @@
 #include "chessboard.h"
 
+cv::Mat chessboard::create(const cv::Size& imgSize, const cv::Size& boardSize,
+    std::vector<cv::Point2f>& imgSpaceCorners)
+{
+    int offset = 50;
+    unsigned char color = 255;
+    cv::Mat board(imgSize, CV_8UC3, cv::Scalar::all(255));
+    int blockWidth
+        = floor((imgSize.height - 2 * offset) / (boardSize.height + 1));
+    int maxWidth = (blockWidth * (boardSize.width + 1)) + (2 * offset);
+
+    std::cout << "--      image height: " << imgSize.height << std::endl;
+    std::cout << "--      image  width: " << imgSize.width << std::endl;
+    std::cout << "-- chessboard height: " << boardSize.height << std::endl;
+    std::cout << "--  chessboard width: " << boardSize.width << std::endl;
+
+    for (int y = offset; y < imgSize.height - offset; y = y + blockWidth) {
+        if (y + blockWidth > imgSize.height - offset) {
+            break;
+        }
+        for (int x = offset; x < imgSize.width - offset; x = x + blockWidth) {
+            color = ~color;
+            if (x + blockWidth > maxWidth) {
+                break;
+            }
+            // collect image space points
+            if (x > offset && y > offset) {
+                imgSpaceCorners.emplace_back(x, y);
+            }
+            cv::Mat block = board(cv::Rect(x, y, blockWidth, blockWidth));
+            block.setTo(cv::Scalar::all(color));
+        }
+    }
+    return board;
+}
+
 void chessboard::findImageSpaceCorners(std::vector<cv::Mat>& images,
-    std::vector<std::vector<cv::Point2f>>& allFoundCorners, bool showResults)
+    std::vector<std::vector<cv::Point2f>>& imageSpaceCorners, bool show)
 {
     for (auto& image : images) {
-        std::vector<cv::Point2f> pointBuf;
-        bool found = cv::findChessboardCorners(image, cv::Size(9, 6), pointBuf,
+        std::vector<cv::Point2f> corner;
+        bool found = cv::findChessboardCorners(image, cv::Size(9, 6), corner,
             cv::CALIB_CB_ADAPTIVE_THRESH | cv::CALIB_CB_NORMALIZE_IMAGE);
 
         if (found) {
-            allFoundCorners.emplace_back(pointBuf);
+            imageSpaceCorners.emplace_back(corner);
         }
 
-        if (showResults) {
-            cv::drawChessboardCorners(image, cv::Size(9, 6), pointBuf, found);
-            cv::imshow("Looking for Corners", image);
+        if (show) {
+            cv::drawChessboardCorners(image, cv::Size(9, 6), corner, found);
+            cv::imshow("CALIBRATION CORNERS", image);
             cv::waitKey(0);
         }
     }
 }
 
 void chessboard::findWorldSpaceCorners(const cv::Size& boardSize,
-    float squareEdgeLength, std::vector<cv::Point3f>& corners)
+    float blockWidth, std::vector<cv::Point3f>& worldSpaceCorners)
 {
     for (int i = 0; i < boardSize.height; i++) {
         for (int j = 0; j < boardSize.width; j++) {
-            corners.emplace_back(cv::Point3f((float)j * squareEdgeLength,
-                (float)i * squareEdgeLength, 0.0f));
+            worldSpaceCorners.emplace_back(cv::Point3f(
+                (float)j * blockWidth, (float)i * blockWidth, 0.0f));
         }
     }
-}
-
-cv::Mat chessboard::generate(const cv::Size& imgSize, const cv::Size& boardSize,
-    std::vector<cv::Point2f>& corners)
-{
-    // opencv requires white boarders around chessboard
-    cv::Mat imgCheckerboard(imgSize, CV_8UC3, cv::Scalar::all(255));
-
-    // create chessboard using blocks
-    const int OFFSET = 10;
-    const int BLOCK_SIDE_LENGTH
-        = (imgSize.width - (2 * OFFSET)) / boardSize.width + 1;
-    const int LEEWAY = BLOCK_SIDE_LENGTH + OFFSET;
-
-    int height = (BLOCK_SIDE_LENGTH * boardSize.height + 1) + (2 * OFFSET);
-    int width = (BLOCK_SIDE_LENGTH * boardSize.width + 1) + (2 * OFFSET);
-
-    std::cout << "--      image: pixel rows = " << imgSize.height << std::endl;
-    std::cout << "--      image: pixel cols = " << imgSize.width << std::endl;
-    std::cout << "-- chessboard: pixel rows = " << height << std::endl;
-    std::cout << "-- chessboard: pixel cols = " << width << std::endl;
-    std::cout << "--      block: pixel size = " << BLOCK_SIDE_LENGTH
-              << std::endl;
-
-    int count = 0;
-    unsigned char color = 1;
-    for (int y = OFFSET; y <= (height - LEEWAY); y += BLOCK_SIDE_LENGTH) {
-        count++;
-        std::cout << count << std::endl;
-        // color = ~color;
-        // for (int x = OFFSET; x <= width - OFFSET; x = x + BLOCK_SIDE_LENGTH)
-        // {
-        //      color = ~color;
-        //     corners.emplace_back(x, y);
-        //     cv::Mat blocks
-        //             = imgCheckerboard(cv::Rect(x, y, BLOCK_SIDE_LENGTH,
-        //             BLOCK_SIDE_LENGTH));
-        //     blocks.setTo(cv::Scalar::all(color));
-        // }
-    }
-    return imgCheckerboard;
 }
