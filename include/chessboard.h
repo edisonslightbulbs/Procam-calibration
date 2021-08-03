af717edd3bd41d4dad70d308dc93866327cd21d3
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-08-03 08:41:28 +0200

    code review

--- include/chessboard.h
+++ include/chessboard.h
@@ -8,9 +8,8 @@ namespace chessboard {
 const float PHYSICAL_BOARD_BLOCK_WIDTH = 0.02500f;
 const float PROJECTED_BOARD_BLOCK_WIDTH = 0.06050f;
 
-void findImageSpaceCorners(std::vector<cv::Mat>& images,
-    std::vector<std::vector<cv::Point2f>>& imageSpaceCorners,
-    bool show = false);
+void findCameraSpaceCorners(std::vector<cv::Mat>& images,
+    std::vector<std::vector<cv::Point2f>>& corners, bool show = false);
 
 void findWorldSpaceCorners(const cv::Size& boardSize, float blockWidth,
     std::vector<cv::Point3f>& worldSpaceCorners);
