415df8b9d147e55a74dea928161215c9bf4f6a19
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-08-04 14:49:08 +0200

    code review

--- include/usage.h
+++ include/usage.h
@@ -1,19 +1,18 @@
 #ifndef USAGE_H
 #define USAGE_H
 
-
 #define LOADING_CALIBRATION_PARAMETERS 20
 #define FINDING_ARUCO_MARKERS 21
 #define USAGE 22
-#define COMPUTING_CALIBRATION_PARAMETERS 23
-#define MORE_CHESSBOARD_IMAGES_REQUIRED 24
-#define WRITING_CALIBRATION_PARAMETERS 25
+#define CALIBRATING 23
+#define MORE_IMAGES_REQUIRED 24
+#define SAVING_PARAMETERS 25
 
 #define ENTER_KEY 13
 #define ESCAPE_KEY 27
 
 namespace usage {
-    void prompt(const int& code);
+void prompt(const int& code);
 }
 
-#endif //USAGE_H
+#endif // USAGE_H
