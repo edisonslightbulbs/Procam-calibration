7c53fd472eda43ed043516ac7136a5bfc1d2bab6
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-08-04 14:48:54 +0200

    code review

--- include/chessboard.h
+++ include/chessboard.h
@@ -8,13 +8,14 @@ namespace chessboard {
 const float R_BLOCK_WIDTH = 0.02500f;
 const float P_BLOCK_WIDTH = 0.06050f;
 
-void findCameraSpaceCorners(std::vector<cv::Mat>& images,
-    std::vector<std::vector<cv::Point2f>>& corners, bool show = false);
-
-void findWorldSpaceCorners(const cv::Size& boardSize, float blockWidth,
-    std::vector<cv::Point3f>& worldSpaceCorners);
-
 cv::Mat create(const cv::Size& imgSize, const cv::Size& boardSize,
     std::vector<cv::Point2f>& imgSpaceCorners);
+
+void capture(
+    const bool& pass, cv::Mat& src, std::vector<cv::Mat>& chessboardImages);
+
+bool overlay(const cv::Mat& src, cv::Mat dst, const cv::Size& dChessboard,
+    const std::string& window);
 }
+
 #endif // CHESSBOARD_H
