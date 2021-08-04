cc948a535dfc9c3be2758b414520e97287458658
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-08-04 15:02:32 +0200

    fixing indentation

--- include/file.h
+++ include/file.h
@@ -1,14 +1,14 @@
 #ifndef PARAMETERS
 #define PARAMETERS
 
-#include <string>
 #include <opencv2/opencv.hpp>
+#include <string>
 
 namespace parameters {
-    bool write(
-            const std::string& name, cv::Mat cameraMatrix, cv::Mat distanceCoefficients);
+bool write(const std::string& name, cv::Mat cameraMatrix,
+    cv::Mat distanceCoefficients);
 
-    bool read(const std::string& name, cv::Mat& cameraMatrix,
-              cv::Mat& distanceCoefficients);
+bool read(const std::string& name, cv::Mat& cameraMatrix,
+    cv::Mat& distanceCoefficients);
 }
-#endif //PARAMETERS
+#endif // PARAMETERS
