f405a8d61e72474a10d86f1be3339d9a653c1a1a
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-08-07 14:31:44 +0200

    (+) checkAspectRation function

--- include/chessboard.h
+++ include/chessboard.h
@@ -6,7 +6,7 @@
 
 #include <opencv2/opencv.hpp>
 
-using t_RGBD = std::pair<cv::Mat, std::vector<Point>>;
+using t_pCloud = std::pair<cv::Mat, std::vector<Point>>;
 
 namespace chessboard {
 
@@ -17,15 +17,16 @@ cv::Mat create(const cv::Size& imgSize, const cv::Size& boardSize,
     std::vector<cv::Point2f>& imgSpaceCorners);
 
 void capture(
-    const bool& pass, cv::Mat& src, std::vector<cv::Mat>& chessboardImages);
+    const bool& pass, cv::Mat& image, std::vector<cv::Mat>& chessboardImages);
 
 bool overlay(const cv::Mat& src, cv::Mat dst, const cv::Size& dChessboard,
     const std::string& window);
 
-void capture(
-    const bool& pass, t_RGBD& data, std::vector<t_RGBD>& dataCollection);
+void capture(const bool& pass, t_pCloud& data, std::vector<t_pCloud>& pCloud);
 
 void project(const cv::Size& dChessboard);
+
+void checkAspectRatio(const int& width, const int& height);
 }
 
 #endif // CHESSBOARD_H
